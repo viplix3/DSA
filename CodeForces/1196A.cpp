e36a43307d4a2714db1ec37f67547ffdb630c908
Vipin <vipin.sharma2024@gmail.com>
2020-11-04 05:58:23 +0530

    Added 1196A

---
+++ CodeForces/1196A.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/1196/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main(){
+	int q;
+	cin>>q;
+	while(q--){
+		ll a, b, c;
+		cin>>a>>b>>c;
+		ll division = (a+b+c)/2;
+		cout<<division<<endl;
+	}
+	return 0;
+}
