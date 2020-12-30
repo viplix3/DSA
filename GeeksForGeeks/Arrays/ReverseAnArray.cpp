de967cdbbbac5050c65806a4301f5b2922783e19
Vipin Sharma <vipin.sharma2024@gmail.com>
2020-12-30 08:29:57 +0530

    Added code for reversing an array in-place

---
+++ GeeksForGeeks/Arrays/ReverseAnArray.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// void swap(int *element_1, int *element_2)
+// {
+//     int *temp = element_1;
+//     element_1 = element_2;
+//     element_2 = temp;
+// }
+
+void reverseArray(int arr[], int size)
+{
+    int begin = 0;
+    int end = size-1;
+
+    while(begin < end){
+        swap(arr[begin], arr[end]);
+        begin++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for(int i=0; i<size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main(){
+    int test_array_1[] = {1, 2, 3, 4, 5};
+    int test_array_2[] = {5, 4, 3, 2, 1};
+    int test_array_3[] = {30, 7, 6, 5, 10};
+
+    printf("\nContents of test array\n");
+    printArray(test_array_1, 5);
+    reverseArray(test_array_1, 5);
+    printf("Contents of reversed array\n");
+    printArray(test_array_1, 5);
+
+    printf("\nContents of test array\n");
+    printArray(test_array_2, 5);
+    reverseArray(test_array_2, 5);
+    printf("Contents of reversed array\n");
+    printArray(test_array_2, 5);
+
+    printf("\nContents of test array\n");
+    printArray(test_array_3, 5);
+    reverseArray(test_array_3, 5);
+    printf("Contents of reversed array\n");
+    printArray(test_array_3, 5);
+
+    return 0;
+}
