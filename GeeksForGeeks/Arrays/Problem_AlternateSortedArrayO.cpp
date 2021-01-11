395d3c736382eb53aa25ae1a94f9c80902741f79
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-01-11 12:42:33 +0530

    O(1) extra space solution is diffuclt af.

---
+++ GeeksForGeeks/Arrays/Problem_AlternateSortedArrayO.cpp
@@ -0,0 +1,81 @@
+// O(N) extra space solutuion -> Super easy
+// O(1) extra space solution -> Super easy, I cheated (https://www.youtube.com/watch?v=ZRoVWxBngX0&feature=emb_logo). 
+
+// C++ program to rearrange an array in minimum 
+// maximum form 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// This function wants you to modify the given input
+// array and no need to return anything
+// arr: input array
+// n: size of array
+void rearrange(long long *arr, int n) 
+{ 
+	
+    int max_idx = n - 1, min_idx = 0;
+    
+    // store maximum element of array
+    int max_elem = arr[n - 1]+1;
+    
+    // traverse array elements
+    for (int i = 0; i < n; i++) {
+        // at even index : we have to put
+        // maximum element
+        if (i % 2 == 0) {
+        arr[i] += (arr[max_idx] % max_elem) * max_elem;
+        max_idx--;
+        }
+    
+        // at odd index : we have to put minimum element
+        else {
+            arr[i] += (arr[min_idx] % max_elem) * max_elem;
+            min_idx++;
+        }
+    }
+    
+    // array elements back to it's original form
+    for (int i = 0; i < n; i++)
+        arr[i] = arr[i] / max_elem;
+	 
+}
+
+// { Driver Code Starts.
+
+// Driver program to test above function 
+int main() 
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        
+        //size of array
+        int n;
+        cin >> n;
+        
+        long long arr[n];
+        
+        //adding elements to the array
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        //calling rearrange() function
+        rearrange(arr, n);
+        
+        //printing the elements
+        for (int i = 0; i < n; i++) 
+		    cout << arr[i] << " ";
+		
+		cout << endl;
+    }
+	return 0; 
+} 
+  // } Driver Code Ends
