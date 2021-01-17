0b5f8520b34c7aafe8bad2913474ecd864a3d7c5
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-01-17 15:07:44 +0530

    Okay, recursion is difficult af

---
+++ Recursion/JosephusProblem.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solveJosephusProblem(int numRemainingPersons, int killIdx)
+{
+    if(numRemainingPersons==1){
+        return 0;
+    }
+    return (solveJosephusProblem(numRemainingPersons-1, killIdx) + killIdx) % numRemainingPersons;
+}
+
+int main(){
+
+    int numPersons, numKillingIdx;
+    while(true){
+        printf("Enter number of persons in the game and the killing index: ");
+        scanf("%d %d", &numPersons, &numKillingIdx);
+        printf("\nIndex of remaining person after the game is finished: %d\n", solveJosephusProblem(numPersons, numKillingIdx));
+    }
+
+    return 0;
+}
