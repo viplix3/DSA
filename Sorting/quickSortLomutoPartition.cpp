9375c0083e274bbb3129e3a708b4d09c9a758a91
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-05-28 06:59:12 +0530

    Easy stuff when using Lomuto partition

---
+++ Sorting/quickSortLomutoPartition.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lomutoPartition(vector<int> &arr, int lowIdx, int highIdx)
+{
+    int pivot = arr[highIdx];
+    int pivotPosition = lowIdx-1;
+
+    for(int i=lowIdx; i<=highIdx-1; i++){
+        if(arr[i] < pivot){
+            pivotPosition += 1;
+            swap(arr[pivotPosition], arr[i]);
+        }
+    }
+    swap(arr[pivotPosition+1], arr[highIdx]);
+    return pivotPosition+1;
+}
+
+void quickSort(vector<int> &arr, int lowIdx, int highIdx)
+{
+    if(lowIdx < highIdx){
+        int partitionIdx = lomutoPartition(arr, lowIdx, highIdx);
+        quickSort(arr, lowIdx, partitionIdx-1);
+        quickSort(arr, partitionIdx+1, highIdx);
+    }
+
+}
+
+void printArray(vector<int> arr)
+{
+    for(int i=0; i<arr.size(); i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main(){
+    int numElements;
+    vector<int> arr;
+
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &numElements);
+    
+    printf("\nEnter the array: ");
+    for(int i=0; i<numElements; i++){
+        int temp;
+        scanf("%d", &temp);
+        arr.push_back(temp);
+    }
+    
+    printf("\nInput array: ");
+    printArray(arr);
+
+    printf("\nSorted array: ");
+    quickSort(arr, 0, arr.size()-1);
+    printArray(arr);
+
+    return 0;
+}
