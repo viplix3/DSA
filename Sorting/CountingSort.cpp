111e238a984e6c76cba4c7bf3f0a4b4e452987a2
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-05-31 06:31:17 +0530

    Chnages, works with negative inputs now.

--- Sorting/CountingSort.cpp
+++ Sorting/CountingSort.cpp
@@ -1,29 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int getMaxElem(vector<int> &arr)
-{
-    int maxElem = INT_MIN;
-    for(int i=0; i<arr.size(); i++)
-        maxElem = max(maxElem, arr[i]);
-    return maxElem;
-}
-
 void countingSort(vector<int> &arr)
 {
-    int maxElem = getMaxElem(arr);
-    maxElem += 1;
-    int counter[maxElem];
-    memset(counter, 0, sizeof(counter));
+    int max = *max_element(arr.begin(), arr.end());
+    int min = *min_element(arr.begin(), arr.end());
+    int range = max - min + 1;
+
+    vector<int> counter(range), outputArr(arr.size());
 
     for(int i=0; i<arr.size(); i++)
-        counter[arr[i]] += 1;
+        counter[arr[i] - min] += 1;
 
-    for(int i=1; i<maxElem; i++)
+    for(int i=1; i<counter.size(); i++)
         counter[i] = counter[i-1] + counter[i]; // counter[i] holds number of elements <= i
     
-    int outputArr[arr.size()];
-
     for(int i=arr.size()-1; i>=0; i--){
         // We use arr[i] to get the number of elements <= arr[i]
         // If arr[i] == 1, then counter[arr[i]] would give us number of elements <= 1
@@ -31,8 +22,8 @@ void countingSort(vector<int> &arr)
         // This tells us that we should put 1 at index 3 (0 based indexing). 
         // As we have used up 1 once, counter[1] should be reduced.
         // By doing so, our algorithm is stable and can be applied to multiple types of input (i.e. if arr are keys) rather than just int
-        outputArr[counter[arr[i]] - 1] = arr[i];
-        counter[arr[i]] -= 1;
+        outputArr[counter[arr[i] - min] - 1] = arr[i];
+        counter[arr[i] - min] -= 1;
     }
 
     for(int i=0; i<arr.size(); i++)
