1ba78ed96c78ba1d7ce6cfa267d5314574d3be77
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-03 07:11:00 +0530

    Cannot solve this!!

--- Sorting/ArraySegregationThreeTypeElements.cpp
+++ Sorting/ArraySegregationThreeTypeElements.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void segrerateArray(vector<int> &arr, int pivot_1, int pivot_2)
+void segrerateArray_consecutivePivots(vector<int> &arr, int pivot_1, int pivot_2)
 {
     int lowIdx = -1, midIdx = 0, highIdx = arr.size();
     
@@ -17,6 +17,28 @@ void segrerateArray(vector<int> &arr, int pivot_1, int pivot_2)
             highIdx--;
             swap(arr[midIdx], arr[highIdx]);
         }
+    }
+}
+
+void segrerateArray(vector<int> &arr, int pivot_1, int pivot_2)
+{
+    if((pivot_2 - pivot_1) == 1)
+        return segrerateArray_consecutivePivots(arr, pivot_1, pivot_2);
+
+    int lowIdx = 0, midIdx = 0, highIdx = arr.size()-1;
+    
+    while(midIdx <= highIdx){
+        if(arr[midIdx] < pivot_1){
+            swap(arr[lowIdx], arr[midIdx]);
+            lowIdx++;
+            midIdx++;
+        }
+        else if(arr[midIdx] > pivot_2){
+            swap(arr[midIdx], arr[highIdx]);
+            highIdx--;
+        }
+        else
+            midIdx++;
     }
 }
 
@@ -55,8 +77,16 @@ int main(){
     printf("Array after segreration w.r.t. pivots %d and %d: ", pivot_1, pivot_2);
     printArray(test_array);
 
+    test_array = {1, 2, 3, 3, 4};
+    pivot_1 = 1, pivot_2 = 2;
+    printf("\nInput array: ");
+    printArray(test_array);
+    segrerateArray(test_array, pivot_1, pivot_2);
+    printf("Array after segreration w.r.t. pivots %d and %d: ", pivot_1, pivot_2);
+    printArray(test_array);
+
     test_array = {10, 5, 6, 3, 20, 9, 40};
-    pivot_1 = 5, pivot_2 = 6;
+    pivot_1 = 5, pivot_2 = 9;
     printf("\nInput array: ");
     printArray(test_array);
     segrerateArray(test_array, pivot_1, pivot_2);
