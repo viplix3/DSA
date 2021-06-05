27a9f42e5edfa276d8573806d821431e33d803a0
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-05 09:38:32 +0530

    Place first n smallest elemnts in smallest array, sort individually

---
+++ Sorting/MergeWithoutExtraSpace.cpp
@@ -0,0 +1,82 @@
+// https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1/
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+        int bound_idx(long long arr[], int startIdx, int endIdx, long long key)
+        {
+            int N = endIdx;
+            while(startIdx < endIdx){
+                int midIdx = startIdx + (endIdx - startIdx) / 2;
+                if(arr[midIdx] <= key)
+                    startIdx = midIdx+1;
+                else
+                    endIdx = midIdx;
+            }
+            if(startIdx < N && arr[startIdx] <= key) {
+                   startIdx++;
+                }
+            return startIdx;
+        }
+        
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int n, int m) 
+        { 
+            int itr_1 = n-1, itr_2 = 0;
+            
+            while(itr_1 >= 0 &&  itr_2 < m){
+                if(arr1[itr_1] >= arr2[itr_2]){
+                    swap(arr1[itr_1], arr2[itr_2]);
+                    itr_1--;
+                    itr_2++;
+                }
+                else
+                    itr_2++;
+            }
+            sort(arr1, arr1+n);
+            sort(arr2, arr2+m);
+        } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
