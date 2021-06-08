a1cd332e55a8700e72bdfa9119181a2c879c62ad
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-08 05:59:32 +0530

    Okay this is a nice concept!

---
+++ Hashing/SubArrayWithGivenSum.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool subArrayWithGivenSum(vector<int> &arr, int sum)
+{
+    unordered_set<int> prefixSumHashTable;
+    int prefixSum = arr[0];
+    prefixSumHashTable.insert(prefixSum);
+
+    for(int i=1; i<arr.size(); i++){
+        prefixSum += arr[i];
+        if(prefixSum == sum)
+            return true;
+        if(prefixSumHashTable.find(prefixSum - sum) != prefixSumHashTable.end())
+            return true;
+        prefixSumHashTable.insert(prefixSum);
+    }
+    return false;
+}
+
+void printArray(vector<int> arr)
+{
+    for(int i=0; i<arr.size(); i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main(){
+    vector<int> test_array;
+    int sum;
+
+    test_array = {5, 8, 6, 13, 3, -1};
+    sum = 22;
+    printf("\nInput array: ");
+    printArray(test_array);
+    if(subArrayWithGivenSum(test_array, sum))
+        printf("Sub-array with sum %d exists in the given array\n", sum);
+    else
+        printf("Sub-array with sum %d doesn't exist in the the given array\n", sum);
+
+    test_array = {15, 2, 8, 10, -5, -8, 6};
+    sum = 3;
+    printf("\nInput array: ");
+    printArray(test_array);
+    if(subArrayWithGivenSum(test_array, sum))
+        printf("Sub-array with sum %d exists in the given array\n", sum);
+    else
+        printf("Sub-array with sum %d doesn't exist in the the given array\n", sum);
+
+    test_array = {3, 2, 5, 6};
+    sum = 10;
+    printf("\nInput array: ");
+    printArray(test_array);
+    if(subArrayWithGivenSum(test_array, sum))
+        printf("Sub-array with sum %d exists in the given array\n", sum);
+    else
+        printf("Sub-array with sum %d doesn't exist in the the given array\n", sum);
+
+    test_array = {5, 3, 2, -1};
+    sum = 4;
+    printf("\nInput array: ");
+    printArray(test_array);
+    if(subArrayWithGivenSum(test_array, sum))
+        printf("Sub-array with sum %d exists in the given array\n", sum);
+    else
+        printf("Sub-array with sum %d doesn't exist in the the given array\n", sum);
+
+    return 0;
+}
