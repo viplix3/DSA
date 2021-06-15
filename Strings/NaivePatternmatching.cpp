c6afd9abf76bd7f1f627dcd5abc4f65713ade541
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-15 06:29:01 +0530

    Corrections

--- Strings/NaivePatternmatching.cpp
+++ Strings/NaivePatternmatching.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> naive_patter_matching(string input_str, string pattern)
+vector<int> naive_pattern_matching(string input_str, string pattern)
 {
 	vector<int> matched_indice;
 
-	for(int i=0; i <= input_str.length() + pattern.length(); i++){
+	for(int i=0; i <= input_str.length() - pattern.length(); i++){
 		int num_matched_chars;
 
 		for(num_matched_chars=0; num_matched_chars<pattern.length(); num_matched_chars++)
@@ -32,7 +32,7 @@ int main(){
 
 	input_str = "ABABABCD";
 	pattern = "ABAB";
-	pattern_occ_idx = naive_patter_matching(input_str, pattern);
+	pattern_occ_idx = naive_pattern_matching(input_str, pattern);
 	cout << "\nInput string: " << input_str << endl;
 	cout << "Search pattern: " << pattern << endl;
 	if(pattern_occ_idx.size() > 0){
@@ -44,7 +44,7 @@ int main(){
 
 	input_str = "ABCABCD";
 	pattern = "ABDC";
-	pattern_occ_idx = naive_patter_matching(input_str, pattern);
+	pattern_occ_idx = naive_pattern_matching(input_str, pattern);
 	cout << "\nInput string: " << input_str << endl;
 	cout << "Search pattern: " << pattern << endl;
 	if(pattern_occ_idx.size() > 0){
@@ -56,7 +56,7 @@ int main(){
 
 	input_str = "AAAAA";
 	pattern = "AAA";
-	pattern_occ_idx = naive_patter_matching(input_str, pattern);
+	pattern_occ_idx = naive_pattern_matching(input_str, pattern);
 	cout << "\nInput string: " << input_str << endl;
 	cout << "Search pattern: " << pattern << endl;
 	if(pattern_occ_idx.size() > 0){
@@ -68,7 +68,7 @@ int main(){
 
 	input_str = "ABCDABCDABC";
 	pattern = "ABCD";
-	pattern_occ_idx = naive_patter_matching(input_str, pattern);
+	pattern_occ_idx = naive_pattern_matching(input_str, pattern);
 	cout << "\nInput string: " << input_str << endl;
 	cout << "Search pattern: " << pattern << endl;
 	if(pattern_occ_idx.size() > 0){
