98da8c64b13c647f8ae3018a4fc41a315c1c8d04
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-16 06:25:27 +0530

    O(N^3) really weird solution

--- Strings/LongestProperPrefixSuffixArray.cpp
+++ Strings/LongestProperPrefixSuffixArray.cpp
@@ -23,3 +23,76 @@ idx 7: PP [a, ab, aba, abac, abaca, abacab, abacaba]	S [d, ad, bad, abad, cabad,
 
 LPS: [0, 0, 1, 0, 1, 2, 3, 0]
 */
+
+int longestProperPrefixSuffix(string str, int strlen)
+{
+	for(int len=strlen-1; len > 0; len--){
+		bool flag = true;
+
+		for(int i=0; i<len; i++)
+			if(str[i] != str[strlen - len + i])
+				flag = false;
+		
+		if(flag)
+			return len;
+	}
+	return 0;
+}
+
+void fillLPS(string str, vector<int> &lps_arr)
+{
+	lps_arr.clear();
+	for(int i=0; i<str.length(); i++){
+		lps_arr.emplace_back(longestProperPrefixSuffix(str, i+1));
+	}
+}
+
+void printArray(vector<int> arr)
+{
+	for(auto ele: arr)
+		cout << ele << " ";
+	cout << endl;
+}
+
+int main(){
+	string str;
+	vector<int> lps_arr;
+
+	str = "ababc";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 0, 1, 2, 0]
+
+	str = "aaaa";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 1, 2, 3]
+
+	str = "abcd";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 0, 0, 0]
+
+	str = "ababab";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 0, 1, 2, 3, 4]
+
+	str = "abacabad";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 0, 1, 0, 1, 2, 3, 0]
+
+	str = "abbabb";
+	cout << "\nInput string: " << str << endl;
+	fillLPS(str, lps_arr);
+	cout << "LPS array: ";
+	printArray(lps_arr); // [0, 0, 0, 1, 2, 3]
+
+	return 0;
+}
