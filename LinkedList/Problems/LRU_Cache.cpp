3debf6417a286cc3319b9a901e7a8db4d7f33184
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-08-05 06:36:36 +0530

    Dummy pointers + Doubly LL + Hashing = LRU!

--- LinkedList/Problems/LRU_Cache.cpp
+++ LinkedList/Problems/LRU_Cache.cpp
@@ -17,47 +17,28 @@ class doublyLL {
 		:key(k), value(v), prev(NULL), next(NULL)
 		{}
 
-    static void add_to_head(doublyLL **headRef, doublyLL **tailRef, doublyLL *node)
+    static void add_to_head(doublyLL **headRef, doublyLL *node)
     {
         doublyLL *head = *headRef;
-        doublyLL *tail = *tailRef;
 
-        node->next = head;
-        if(head != NULL)
-            node->prev = head->prev;
-
-        head = node;
-        if(tail == NULL)
-            tail = head;
-
-        *headRef = head;
-        *tailRef = tail;
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
     }
 
     static void remove_node_from_current_pos(doublyLL *node)
     {
-        doublyLL *prev = node->prev;
-
-        if(prev != NULL)
-            prev->next = node->next;
-        
-        if(node->next != NULL)
-            node->next->prev = node->prev;
-
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
         node->next = NULL;
         node->prev = NULL;
     }
 
     static void delete_node(doublyLL *node)
     {
-        doublyLL *prev = node->prev;
-
-        if(prev != NULL)
-            prev->next = node->next;
-        
-        if(node->next != NULL)
-            node->next->prev = node->prev;
-        
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
         delete node;
     }
 };
@@ -76,6 +57,16 @@ class LRUCache
     {
         capacity = cap;
         count = 0;
+        
+        // Throw away nodes
+        head = new doublyLL(INT_MIN, INT_MIN);
+        tail = new doublyLL(INT_MAX, INT_MAX);
+        head->next = tail;
+        tail->prev = head;
+
+        key_node_map[INT_MIN] = head;
+        key_node_map[INT_MAX] = tail;
+
     }
     
     //Function to return value corresponding to the key.
@@ -87,7 +78,7 @@ class LRUCache
 
             // Move the node to the front of doubly LL
             doublyLL::remove_node_from_current_pos(node);
-            doublyLL::add_to_head(&head, &tail, node);
+            doublyLL::add_to_head(&head, node);
 
             return out_data;
         }
@@ -105,14 +96,14 @@ class LRUCache
 
             // Move the node to front of doubly LL
             doublyLL::remove_node_from_current_pos(node);
-            doublyLL::add_to_head(&head, &tail, node);
+            doublyLL::add_to_head(&head, node);
         }
         else { // Key is not present
             doublyLL *new_node = new doublyLL(key, value);
             key_node_map[key] = new_node;
             if(count < capacity) { // Doubly LL is not full, simply add node to head
                 count += 1;
-                doublyLL::add_to_head(&head, &tail, new_node);
+                doublyLL::add_to_head(&head, new_node);
             }
             else { // Doubly LL is full
                 // Remove the least recently used data
@@ -120,7 +111,7 @@ class LRUCache
                 doublyLL::delete_node(tail->prev);
 
                 // Add a new node to head
-                doublyLL::add_to_head(&head, &tail, new_node);
+                doublyLL::add_to_head(&head, new_node);
             }
         }
 
