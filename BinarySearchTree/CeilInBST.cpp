d1ca224e0912051e75f65a2c812e307f0a3ce070
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-08-31 08:14:51 +0530

    BST search with slight modifications

---
+++ BinarySearchTree/CeilInBST.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace BST {
+
+	struct Node {
+		int data;
+		Node *left, *right;
+
+		Node(int x)
+			: data(x), left(NULL), right(NULL)
+		{}
+	};
+
+
+	bool search(Node* root, int key) {
+		while(root != NULL) {
+			if(root->data == key)
+				return true;
+
+			else if (root->data > key)
+				root = root->left;
+			else
+				root = root->right;
+		}
+
+		return false;
+	}
+
+	Node* insert(Node* root, int data) {
+		Node *new_node = new Node(data);
+
+		if(root == NULL)
+			return new_node;
+
+		Node *curr = root, *prev = NULL;
+
+		while(curr != NULL) {
+			if(curr-> data == new_node->data)
+				break;
+
+			prev = curr;
+
+			if(curr->data < new_node->data)
+				curr = curr->right;
+			else
+				curr = curr->left;
+		}
+
+		if(prev == NULL)
+			return root;
+		
+		else if(prev->data < new_node->data)
+			prev->right = new_node;
+		else
+			prev->left = new_node;
+
+		return root;
+	}
+
+	int getCeil(Node* root, int data) {
+		int ceil = -1;
+
+		while(root != NULL) {
+			if(root->data == data)
+				return root->data;
+			else if(root->data > data) {
+				ceil = root->data;
+				root =  root->left;
+			}
+			else if(root->data < data)
+				root = root->right;
+		}
+
+		return ceil;
+	}
+}
+
+int main() {
+	BST::Node *root = NULL;
+	int search_data, floor_value;
+
+	root = BST::insert(root, 10);
+	root = BST::insert(root, 5);
+	root = BST::insert(root, 15);
+	root = BST::insert(root, 12);
+	root = BST::insert(root, 30);
+
+	search_data = 14;
+	floor_value = BST::getCeil(root, search_data);
+	if(floor_value == -1)
+		cout << "Ceil for value " << search_data << " doesn't exist" << endl;
+	else
+		cout << "Ceil for value " << search_data << " in the provided BST: " << floor_value << endl;
+
+	search_data = 3;
+	floor_value = BST::getCeil(root, search_data);
+	if(floor_value == -1)
+		cout << "Ceil for value " << search_data << " doesn't exist" << endl;
+	else
+		cout << "Ceil for value " << search_data << " in the provided BST: " << floor_value << endl;
+
+	search_data = 40;
+	floor_value = BST::getCeil(root, search_data);
+	if(floor_value == -1)
+		cout << "Ceil for value " << search_data << " doesn't exist" << endl;
+	else
+		cout << "Ceil for value " << search_data << " in the provided BST: " << floor_value << endl;
+
+	search_data = 11;
+	floor_value = BST::getCeil(root, search_data);
+	if(floor_value == -1)
+		cout << "Ceil for value " << search_data << " doesn't exist" << endl;
+	else
+		cout << "Ceil for value " << search_data << " in the provided BST: " << floor_value << endl;
+
+	return 0;
+}
