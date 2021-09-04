77a82f39c9051b55cb76288e087be978c0e294ab
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-09-04 08:30:35 +0530

    InOrder traversal + 2 pointer

---
+++ BinarySearchTree/PairSumBST.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+namespace BST {
+	struct Node {
+		int data;
+		Node *left, *right;
+
+		Node(int x)
+			: data(x), left(NULL), right(NULL)
+		{}
+	};
+
+	// InOrder traversal based method
+	bool pairSum(Node* root, int sum) {
+		if(root == NULL)
+			return false;
+		
+		// Iterative inOrder
+		vector<Node*> inOrder;
+		stack<Node*> prevNode;
+		Node *curr = root;
+		while(curr != NULL || prevNode.empty() == false) {
+			while(curr != NULL) {
+				prevNode.push(curr);
+				curr = curr->left;
+			}
+			curr = prevNode.top();
+			prevNode.pop();
+			inOrder.push(curr);
+			curr = curr->right;
+		}
+
+		int startIdx = 0, endIdx = inOrder.size()-1;
+		while(startIdx < endIdx) {
+			int beginData = inOrder[startIdx]->data;
+			int endData = inOrder[endIdx]->data;
+
+			if((beginData + endData) < sum)
+				startIdx += 1;
+			else if((beginData + endData) > sum)
+				endIdx -= 1;
+			else
+				return true;
+		}
+		return false;
+	}
+}
