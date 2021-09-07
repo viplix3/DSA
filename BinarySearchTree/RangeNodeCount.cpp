29b6980a63c895ae76276be3a04b33ec00ed57c3
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-09-07 08:25:08 +0530

    Added GFG solution, much easier to understand!

--- BinarySearchTree/RangeNodeCount.cpp
+++ BinarySearchTree/RangeNodeCount.cpp
@@ -108,7 +108,7 @@ int main()
 
 
 //Function to count number of nodes in BST that lie in the given range.
-int getCount(Node *root, int lowerBound, int upperBound)
+int getCountComplex(Node *root, int lowerBound, int upperBound)
 {
     if(root == NULL)
         return 0;
@@ -128,4 +128,28 @@ int getCount(Node *root, int lowerBound, int upperBound)
         rangeNodeCount += getCount(root->right, lowerBound, upperBound);
     
     return (root_in_range + rangeNodeCount);
+}
+
+//Function to count number of nodes in BST that lie in the given range.
+int getCount(Node* root, int low, int high)
+{
+	if (!root) 
+	    return 0;
+
+	//if data at current node is equal to lower and upper range, we return 1.
+	if (root->data == high && root->data == low)
+		return 1;
+
+	//if data at current node is within range then we include it in count 
+	//and call function recursively for its left and right children.
+	if (root->data <= high && root->data >= low)
+		return 1+getCount(root->left,low,high)+getCount(root->right,low,high);
+
+	//else if data at current node is smaller than lower range then
+	//we call function recursively only for right child.
+	else if (root->data < low)
+		return getCount(root->right, low, high);
+
+	//else we call function recursively only for left child.
+	else return getCount(root->left, low, high);
 }
