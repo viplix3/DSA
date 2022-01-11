a55dc1ee87ca4f861dbe4dd05f9c9ba09f5725ec
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-01-11 07:27:16 +0530

    Revision: Some comments for better understanding

--- Hashing/MoreThanN_K_occurrences_small_k.cpp
+++ Hashing/MoreThanN_K_occurrences_small_k.cpp
@@ -6,14 +6,18 @@ unordered_map<int, int> modified_moore_voting(vector<int> &arr, int k)
     unordered_map<int, int> frequency_map;
 
     for(int i=0; i<arr.size(); i++){
+        // If element is already present in the map, increase its voting count
         if(frequency_map.find(arr[i]) != frequency_map.end())
             frequency_map[arr[i]] += 1;
+
+        // If map has space to accomodate more elements, add the current element in the map
         else if(frequency_map.size() < k-1)
             frequency_map[arr[i]] += 1;
-        else
+
+        else // Neither map has the current element, nor there is any space left in the map
             for(auto itr: frequency_map){
-                itr.second -=1;
-                if(itr.second == 0)
+                itr.second -=1; // Current element would reduce count of all the elements in the map
+                if(itr.second == 0) // If count of any element is reduced to 0, it is removed
                     frequency_map.erase(itr.first);
             }
     }
@@ -22,9 +26,14 @@ unordered_map<int, int> modified_moore_voting(vector<int> &arr, int k)
 
 vector<int> get_nk_occurrence_elements(vector<int> &arr, int k)
 {
+    /*
+        If count is the number of elements in the results then
+            count <= (K - 1)
+    */
     unordered_map<int, int> frequency_map = modified_moore_voting(arr, k);
     vector<int> out_elements;
 
+    // Select/ Reject the condidates on the basis of actual occurrence conts
     for(auto itr: frequency_map){
         itr.second = 0;
         for(int i=0; i<arr.size(); i++)
