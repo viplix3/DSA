f99b9f2cb2f9ec8e2b8c2ebe011bb2c7e2a3db3b
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-07-17 16:01:19 +0400

    Added O(N^2) solution

--- LeetCode/00036_ValidSudoku.cpp
+++ LeetCode/00036_ValidSudoku.cpp
@@ -43,3 +43,42 @@ public:
         return true;
     }
 };
+
+// Optimized O(N^2) solution from discussion
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        const int boardSize = 9;
+        int rowMask[9][9] = {0}, colMask[9][9] = {0}, gridMask[9][9] = {0};
+        
+        
+        for(int rowIdx = 0; rowIdx < boardSize; rowIdx++) {
+            for(int colIdx = 0; colIdx < boardSize; colIdx++) {
+                if(board[rowIdx][colIdx] != '.') {
+                    int currDigit = board[rowIdx][colIdx] - '0';
+                    
+                    // currDigit appeared in current row already
+                    if(rowMask[rowIdx][currDigit-1] == 1)
+                        return false;
+                    
+                    // currDigit appeared in current col already
+                    if(colMask[colIdx][currDigit-1] == 1)
+                        return false;
+                    
+                    // currDigit appeared in current 3x3 grid already
+                    // 1 whole 3x3 grid would be addressed by a single index (initial index of the grid)
+                    // We can check for occurence of 9 numbers in the gird index
+                    int flattennedGridIdx = (3 * (rowIdx / 3)) + (colIdx / 3);
+                    if(gridMask[flattennedGridIdx][currDigit-1] == 1)
+                        return false;
+                    
+                    // Digit didn't appear before, populate in the mask to mark occurrence
+                    rowMask[rowIdx][currDigit-1] = colMask[colIdx][currDigit-1] = 
+                        gridMask[flattennedGridIdx][currDigit-1] = 1;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
