686daf5d603131c432a149dc252118ac519b1552
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-07-22 18:29:40 +0400

    2 pointer: More efficient solution

--- LeetCode/00042_TrappingRainWater.cpp
+++ LeetCode/00042_TrappingRainWater.cpp
@@ -1,5 +1,43 @@
 // https://leetcode.com/problems/trapping-rain-water/
 
+// O(N) time, O(1) space
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int numBars = height.size(), trappedRainWater = 0;
+        
+        if(numBars <= 2)
+            return trappedRainWater;
+        
+        int leftSupportIdx = 1, leftSupportValue = height[0];
+        int rightSupportIdx = numBars - 2, rightSupportValue = height[numBars - 1];
+        
+        while(leftSupportIdx <= rightSupportIdx) {
+            // In each iteration we will move from lower support to higher support
+            // Doint so can potentially give land us on heights having water on top of them
+            if(leftSupportValue < rightSupportValue) {
+                // Current height is greater than previous left support, hence cannot hold any water on top of it
+                if(height[leftSupportIdx] > leftSupportValue)
+                    leftSupportValue = height[leftSupportIdx];
+                else
+                    // Left support is greater than current current height, add the trapped water
+                    trappedRainWater += leftSupportValue - height[leftSupportIdx];
+                leftSupportIdx++;
+            }
+            else { // Do same for right
+                if(height[rightSupportIdx] > rightSupportValue)
+                    rightSupportValue = height[rightSupportIdx];
+                else
+                    trappedRainWater += rightSupportValue - height[rightSupportIdx];
+                rightSupportIdx--;
+            }
+        }
+        
+        return trappedRainWater;
+    }
+};
+
+// O(N) time, O(N) space
 class Solution {
 public:
     int trap(vector<int>& height) {
