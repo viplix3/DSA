86c09153fb9061316abd59284273b5b3739ba289
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-07-31 15:11:47 +0400

    Binary search solution O(N log(N))

--- LeetCode/00287_FindTheDuplicateNumber.cpp
+++ LeetCode/00287_FindTheDuplicateNumber.cpp
@@ -23,6 +23,43 @@ public:
     }
 };
 
+
+// Binary search solution from leetcode tutorial, O(N log(N))
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        // lambda function to count occurrences of number
+        auto countSmallOrEqual = [&](int currSearchNum) {
+            int occurrenceCount = 0;
+            
+            for(auto &currNum : nums) {
+                if(currNum <= currSearchNum)
+                    occurrenceCount += 1;
+            }
+            
+            return occurrenceCount;
+        };
+        
+        int searchInit = 1, searchEnd = nums.size();
+        int repeatingNum = -1;
+        while(searchInit <= searchEnd) {
+            int searchNum = searchInit + (searchEnd - searchInit) / 2;
+            
+            // This number is repeating
+            if(countSmallOrEqual(searchNum) > searchNum) {
+                repeatingNum = searchNum;
+                searchEnd = searchNum - 1; // Try to find a smaller repeating number in next iteration
+            }
+            else
+                searchInit = searchNum + 1; // Couldn't find a repeating number, try different range
+        }
+        
+        return repeatingNum;
+    }
+};
+
+
 // Find occurrence count of each element: O(N), modifies the array
 class Solution {
 public:
