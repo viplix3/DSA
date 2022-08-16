d14c5582721f8a8787824026183b7d5a952c3a0a
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-08-16 07:41:45 +0400

    The solution was not correct

--- LeetCode/00347_TopKFrequentElements.cpp
+++ LeetCode/00347_TopKFrequentElements.cpp
@@ -5,32 +5,6 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int numElems = nums.size();
-        vector<int> topKElems;
-        unordered_map<int, int> frequencyMap;
-        
-        if(nums.size() == 0)
-            return topKElems;
-        
-        for(int i = 0; i < numElems; i++) {
-            if(frequencyMap.find(nums[i]) != frequencyMap.end()) {
-                frequencyMap[nums[i]] += 1;
-            }
-            else {
-                for(auto itr : frequencyMap) {
-                    itr.second--;
-                    if(itr.second == 0)
-                        frequencyMap.erase(itr.first);
-                }
-            }
-            
-            if(frequencyMap.size() < k)
-                frequencyMap[nums[i]] += 1;
-        }
-        
-        for(auto itr : frequencyMap)
-            topKElems.push_back(itr.first);
-        
-        return topKElems;
+
     }
 };
