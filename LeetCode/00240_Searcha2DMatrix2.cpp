ea40201b3ef526a44e4a421bc9702b61275dc26d
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-08-24 18:54:26 +0400

    Still don't know what is the difference b/w this and search a 2D matrix

---
+++ LeetCode/00240_Searcha2DMatrix2.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int numRows = matrix.size(), numCols = matrix[0].size();
+        int rowIdx = 0, colIdx = numCols - 1;
+        
+        while(rowIdx < numRows && colIdx >= 0) {
+            if(matrix[rowIdx][colIdx] == target)
+                return true;
+            else if(matrix[rowIdx][colIdx] < target)
+                rowIdx++;
+            else
+                colIdx--;
+        }
+        
+        return false;
+    }
+};
