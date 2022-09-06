e71d9b1840857b48fe5c89f4a5c08f09647da885
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-09-06 04:14:15 +0400

    Super important pattern (from leetcode discuss)

--- LeetCode/00076_MinimumWindowSubstring.cpp
+++ LeetCode/00076_MinimumWindowSubstring.cpp
@@ -1,5 +1,62 @@
 // https://leetcode.com/problems/minimum-window-substring/
 
+// This pattern can be used for solving any substring finding problems
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int sSize = s.length(), tSize = t.length();
+        
+        if(tSize > sSize)
+            return "";
+        
+        int windowInitIdx = 0, windowEndIdx = 0, numMatchedChars = 0;
+        int minWindowStart = 0, minWindowLen = sSize + 1;
+        int freqMap[128] = {0};
+        
+        for(char ch : t)
+            freqMap[ch]++;
+        
+        while(windowEndIdx < sSize) { // Window finding loop
+            
+            // If char at windowEndIdx in s also exists in t
+            // Increase the number of matched characters
+            if(freqMap[s[windowEndIdx]] > 0)
+                numMatchedChars++;
+            
+            // If char at windowEndIdx in s exits in t, this will reduce the count by 1
+            // as this character has been counted above
+            // Else, this will make the count negative
+            freqMap[s[windowEndIdx]]--;
+            
+            // If we have found all the matching characters
+            // save the current window metadata and shrink window
+            // until we encounter a character which is in t
+            while(numMatchedChars == tSize) { // Window shrinking loop
+                if(windowEndIdx - windowInitIdx + 1 < minWindowLen) {
+                    minWindowStart = windowInitIdx;
+                    minWindowLen = windowEndIdx - windowInitIdx + 1;
+                }
+                
+                // We are removing the char at windowInitIdx in s
+                // so we increment the count that was decremented outside this loop
+                // The count will be positive if the char was in t
+                // Else this would become 0
+                freqMap[s[windowInitIdx]]++;
+                
+                // We removed a char which was in t
+                if(freqMap[s[windowInitIdx]] > 0)
+                    numMatchedChars--;
+                
+                windowInitIdx++;
+            }
+            
+            windowEndIdx++;
+        }
+        
+        return (minWindowLen == sSize + 1) ? "" : s.substr(minWindowStart, minWindowLen);
+    }
+};
+
 
 // Solution using 2 maps
 class Solution {
