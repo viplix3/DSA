f899c0ba92a04cf13b67ac099b70f54677907cc3
Vipin Sharma <vipin.sharma2024@gmail.com>
2022-09-14 07:07:53 +0400

    People are fukcing genius man

--- LeetCode/00160_IntersectionOfTwoLinkedLists.cpp
+++ LeetCode/00160_IntersectionOfTwoLinkedLists.cpp
@@ -8,6 +8,34 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Taken from: https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/49785/Java-solution-without-knowing-the-difference-in-len!
+// Look at 1st comment for understanding clearly
+class Solution {
+
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB)
+            return nullptr;
+        
+        ListNode *nodeRunnerA = headA, *nodeRunnerB = headB;
+        
+        while(nodeRunnerA != nodeRunnerB) {
+            nodeRunnerA = (nodeRunnerA == nullptr) ? headB : nodeRunnerA->next;
+            nodeRunnerB = (nodeRunnerB == nullptr) ? headA : nodeRunnerB->next;
+        }
+        
+        return nodeRunnerA;
+    }
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 private:
     int getListLength(ListNode* head) {
