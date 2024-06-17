02ec6f1cee60d71ea75933992527f90f23a79bec
Vipin Sharma <vipin.sharma2024@gmail.com>
2024-06-17 13:56:25 +0400

    best solution taken from lc discuss

--- Striver/A2Z_DSA_Sheet/ValidPalindrome.cpp
+++ Striver/A2Z_DSA_Sheet/ValidPalindrome.cpp
@@ -34,7 +34,7 @@ private:
     }
 
 public:
-    bool isPalindrome(string s)
+    bool isPalindrome_recursive(string s)
     {
         string cleaned_str = clean_string(s);
 
@@ -59,6 +59,34 @@ public:
             l_itr++, r_itr--;
         }
 
+        return true;
+    }
+
+public:
+    bool isPalindrome(string s)
+    {
+        int start = 0;
+        int end = s.size() - 1;
+        while (start <= end)
+        {
+            if (!isalnum(s[start]))
+            {
+                start++;
+                continue;
+            }
+            if (!isalnum(s[end]))
+            {
+                end--;
+                continue;
+            }
+            if (tolower(s[start]) != tolower(s[end]))
+                return false;
+            else
+            {
+                start++;
+                end--;
+            }
+        }
         return true;
     }
 };
