3b29b30abaa31273e2a62431c8a73e666509822a
Vipin Sharma <vipin.sharma2024@gmail.com>
2024-06-23 14:55:38 +0400

    discuss solution

--- LeetCode/01838_FrequencyOfTheMostFrequentElement.cpp
+++ LeetCode/01838_FrequencyOfTheMostFrequentElement.cpp
@@ -30,4 +30,22 @@ public:
 
         return max_frequency;
     }
+
+    // The solution from discuss, almost the same but instead of updating k, it only updates operations
+    int maxFrequency(vector<int> &nums, int k)
+    {
+        sort(begin(nums), end(nums));
+        uint l = 0, r = 1, incrementsRequired = 0;
+        for (; r < size(nums); r++)
+        {
+            // increments requried to bring all nums[j] (for l <= j < r) to nums[r]
+            incrementsRequired += (nums[r] - nums[r - 1]) * (r - l);
+            // if increment required become greater than k, then,
+            // reduce the increments required to get nums[l] to nums[r] & slide left pointer forward
+            if (incrementsRequired > k)
+                incrementsRequired -= (nums[r] - nums[l++]);
+        }
+        // finally the [l, r] window will hold the size of maximum number of elements that can be made equal
+        return r - l;
+    }
 };
