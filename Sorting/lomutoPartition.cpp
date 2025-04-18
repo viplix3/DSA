58c97c362a84099cc34bbb55cc95e50dbdc26f59
vipin <vipin.sharma2024@gmail.com>
2025-04-18 05:55:27 +0400

    formatter changes

--- Sorting/lomutoPartition.cpp
+++ Sorting/lomutoPartition.cpp
@@ -1,14 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int lomutoPartition(vector<int> &arr, int pivotIdx, int lowIdx, int highIdx)
 {
     int pivot = arr[pivotIdx];
-    swap(arr[pivotIdx], arr[highIdx]); // Move pivot to last position
-    int pivotPosition = lowIdx-1;
+    swap(arr[pivotIdx], arr[highIdx]);// Move pivot to last position
+    int pivotPosition = lowIdx - 1;
 
-    for(int i=lowIdx; i<=highIdx-1; i++){
-        if(arr[i] < pivot){
+    for (int i = lowIdx; i <= highIdx - 1; i++)
+    {
+        if (arr[i] < pivot)
+        {
             pivotPosition += 1;
             swap(arr[pivotPosition], arr[i]);
         }
@@ -21,12 +23,13 @@ int lomutoPartition(vector<int> &arr, int pivotIdx, int lowIdx, int highIdx)
 
 void printArray(vector<int> arr)
 {
-    for(int i=0; i<arr.size(); i++)
+    for (int i = 0; i < arr.size(); i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-int main(){
+int main()
+{
     vector<int> test_array;
     int pivotIdx;
 
@@ -34,22 +37,22 @@ int main(){
     pivotIdx = 4;
     printf("\nInput array is: ");
     printArray(test_array);
-    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx, 
-        lomutoPartition(test_array, pivotIdx, 0, test_array.size()-1));
+    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx,
+           lomutoPartition(test_array, pivotIdx, 0, test_array.size() - 1));
 
     test_array = {70, 60, 80, 40, 30};
     pivotIdx = 4;
     printf("\nInput array is: ");
     printArray(test_array);
-    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx, 
-        lomutoPartition(test_array, pivotIdx, 0, test_array.size()-1));
+    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx,
+           lomutoPartition(test_array, pivotIdx, 0, test_array.size() - 1));
 
     test_array = {30, 40, 20, 50, 80};
     pivotIdx = 4;
     printf("\nInput array is: ");
     printArray(test_array);
-    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx, 
-        lomutoPartition(test_array, pivotIdx, 0, test_array.size()-1));
+    printf("Position of pivot at index %d in sorted array would be: %d\n", pivotIdx,
+           lomutoPartition(test_array, pivotIdx, 0, test_array.size() - 1));
 
     return 0;
 }
