9b983e6b3f9b30f87a4a4d987aebbfbea6ca807d
vipin <vipin.sharma2024@gmail.com>
2025-04-21 12:56:04 +0400

    removed set (need to remember this element skip trick as I keep encountering it)

--- LeetCode/00015_3Sum.cpp
+++ LeetCode/00015_3Sum.cpp
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/3sum
 
 // Very slow O(N^2 log(K)) solution, K -> number of triplets inserted into the set
-class Solution
+class Solution_Shit
 {
 private:
     vector<pair<int, int>> two_sum(vector<int> &nums, int k, int left_ptr, int right_ptr)
@@ -39,13 +39,70 @@ public:
             if (!two_sum_solutions.empty())
             {
                 for (const pair<int, int> &two_sum_pairs: two_sum_solutions)
+                {
+                    vector<int> triplets = {current_element, two_sum_pairs.first, two_sum_pairs.second};
                     solution_triplets_set.insert({current_element, two_sum_pairs.first, two_sum_pairs.second});
+                }
             }
         }
 
+        vector<vector<int>> solution_triplets(solution_triplets_set.begin(), solution_triplets_set.end());
+        return solution_triplets;
+    }
+};
+
+
+// O(N^2) time
+class Solution
+{
+private:
+    vector<pair<int, int>> two_sum(vector<int> &nums, int k, int left_ptr, int right_ptr)
+    {
+        vector<pair<int, int>> solution_pairs;
+
+        while (left_ptr < right_ptr)
+        {
+            if (nums[left_ptr] + nums[right_ptr] == k)
+            {
+                pair<int, int> solution = {nums[left_ptr], nums[right_ptr]};
+                solution_pairs.push_back(solution);
+
+                // Move until you're not at the same value
+                while (left_ptr < right_ptr && nums[left_ptr] == solution.first)
+                    left_ptr++;
+                while (left_ptr < right_ptr && nums[right_ptr] == solution.second)
+                    right_ptr--;
+            }
+            else if (nums[left_ptr] + nums[right_ptr] < k)
+                left_ptr++;
+            else
+                right_ptr--;
+        }
+
+        return solution_pairs;
+    }
+
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        int num_elements = nums.size();
         vector<vector<int>> solution_triplets;
-        for (vector<int> solution: solution_triplets_set)
-            solution_triplets.emplace_back(solution);
+        int prev_element = INT_MIN;
+        for (int i = 0; i < num_elements - 2; i++)
+        {
+            if (nums[i] == prev_element)
+                continue;
+
+            int current_element = nums[i];
+            vector<pair<int, int>> two_sum_solutions = two_sum(nums, -current_element, i + 1, num_elements - 1);
+            if (!two_sum_solutions.empty())
+                for (const pair<int, int> &two_sum_pairs: two_sum_solutions)
+                    solution_triplets.push_back({current_element, two_sum_pairs.first, two_sum_pairs.second});
+
+            prev_element = current_element;
+        }
 
         return solution_triplets;
     }
